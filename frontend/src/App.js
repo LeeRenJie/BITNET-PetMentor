338247fae3bdf0d70c6177bebcebf53bff864d27
Lee Ren Jie <work.renjie@gmail.com>
2021-02-20 08:58:51 +0800

    Update

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, {component} from "react"
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from "./components/Navbar"
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+      <Navbar />
+      </div>
+    );
+  }
+}
+
+export default App;

c4b3c53fc5cdb76a1b5574cca60427aeaeefcd49
Lee Ren Jie <work.renjie@gmail.com>
2021-02-20 22:07:16 +0800

    Moved files out of backend

--- backend/BitNetPy/testapp/urls.py
+++ backend/BitNetPy/testapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import index
 
 urlpatterns = [
-    path('', views.index)
+    path('', index),
+    path('Dashboard',index),
+    path('Pets', index),
+    path('Stats', index)
 ]
